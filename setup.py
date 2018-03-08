da7fff4857db57ee5523c525682f43acdc018035
Joe Black <me@joeblack.nyc>
2018-03-07 19:04:10 -0500

    version bump

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup, find_packages
 setup(
     name="btcpay",
     packages=find_packages(),
-    version="1.0.2",
+    version="1.0.3",
     description="Accept bitcoin with BTCPay",
     author="Joe Black",
     author_email="me@joeblack.nyc",
     url="https://github.com/joeblackwaslike/btcpay-python",
-    download_url="https://github.com/joeblackwaslike/btcpay-python/tarball/v1.0.2",
+    download_url="https://github.com/joeblackwaslike/btcpay-python/tarball/v1.0.3",
     license='MIT',
     keywords=["bitcoin", "payments", "crypto"],
     install_requires=[
