c45635e56c0c619425ac93890abd0c2c9339ffe4
Jeff Vandrew Jr <jeffvandrew@yahoo.com>
2019-01-17 09:58:45 -0500

    match name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="btcpay_python",
+    name="btcpay-python",
     packages=find_packages(),
     version="1.1.0",
     description="Accept bitcoin with BTCPay",
